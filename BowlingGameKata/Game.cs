c401a189394e24c7a1ca2213ddace7e539a518fa
Ben Skiff <benskiff@msn.com>
2017-05-25 00:15:06 -0500

    Add project files.

---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+        public void roll(int pins)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int score()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BowlingGameKataTests/GameTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGameKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void rollTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void scoreTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
