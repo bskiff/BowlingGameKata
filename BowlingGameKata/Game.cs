68c1e25396b98cf0f5fca8aaa5ea0c22ad28bca0
Ben Skiff <benskiff@msn.com>
2017-05-25 01:06:34 -0500

    WIP

--- BowlingGameKata/Game.cs
+++ BowlingGameKata/Game.cs
@@ -8,14 +8,34 @@ namespace BowlingGameKata
 {
     public class Game
     {
+        private int[] totalPins = new int[21];
+        private int currentPin = 0;
+
         public void roll(int pins)
         {
-            throw new NotImplementedException();
+            totalPins[currentPin++] = pins;
+            if (pins == 10)
+            {
+                currentPin++;
+            }
         }
 
         public int score()
         {
-            throw new NotImplementedException();
+            var total = 0;
+            for (int i = 0; i < 20; i += 2) {
+                var frameScore = totalPins[i] + totalPins[i + 1];
+                if(totalPins[i] == 10)
+                {
+                    total += totalPins[i + 2] + totalPins[i + 3];
+                } else if (frameScore == 10)
+                {
+                    total += totalPins[i + 2];
+                }
+                total += frameScore;
+            }
+
+            return total;
         }
     }
 }
--- BowlingGameKataTests/GameTests.cs
+++ BowlingGameKataTests/GameTests.cs
@@ -11,16 +11,61 @@ namespace BowlingGameKata.Tests
     [TestClass()]
     public class GameTests
     {
-        [TestMethod()]
-        public void rollTest()
+        private Game game;
+
+        private void rollNTimes(int val, int n)
         {
-            Assert.Fail();
+            for (int i = 0; i < n; i++)
+            {
+                game.roll(val);
+            }
+        }
+
+        [TestInitialize]
+        public void setup()
+        {
+            game = new Game();
         }
 
         [TestMethod()]
-        public void scoreTest()
+        public void gutterGame()
         {
-            Assert.Fail();
+            rollNTimes(0, 20);
+            Assert.AreEqual(0, game.score());
+        }
+
+        [TestMethod()]
+        public void onesGame()
+        {
+            rollNTimes(1, 20);
+            Assert.AreEqual(20, game.score());
+        }
+
+        [TestMethod()]
+        public void singleSpare()
+        {
+            game.roll(5);
+            game.roll(5);
+            game.roll(3);
+            rollNTimes(0, 17);
+            Assert.AreEqual(16, game.score());
+        }
+
+        [TestMethod()]
+        public void singleStrike()
+        {
+            game.roll(10);
+            game.roll(6);
+            game.roll(2);
+            rollNTimes(0, 16);
+            Assert.AreEqual(26, game.score());
+        }
+
+        [TestMethod()]
+        public void perfectGame()
+        {
+            rollNTimes(10, 10);
+            Assert.AreEqual(300, game.score());
         }
     }
 }
